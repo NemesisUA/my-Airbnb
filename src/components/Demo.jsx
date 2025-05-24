e45d0d1eb044b8052fa202083b0a694c28b528e5
NemesisUA <display.n0ne.01.gmail.com>
2025-05-24 14:48:50 +0300

    init project

---
+++ src/components/Demo.jsx
@@ -0,0 +1,23 @@
+// import { FC } from "react"
+
+// type DemoProps = {
+//   name: string
+// }
+
+// export const Demo: FC<DemoProps> = ({ name }) => {
+//   return (
+//     <div>
+//       <h1>Demo</h1>
+//       <p>{name}</p>
+//     </div>
+//   )
+// }
+
+export const Demo = ({ name }) => {
+  return (
+    <div>
+      <h1>Demo</h1>
+      <p>{name}</p>
+    </div>
+  );
+};
